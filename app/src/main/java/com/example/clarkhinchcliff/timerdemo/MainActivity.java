475008682558f7b8753616a78b246737298e28ef
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-30 18:19:40 -0700

    countdown timer

--- app/src/main/java/com/example/clarkhinchcliff/timerdemo/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/timerdemo/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
             public void onFinish(){
                 //countdown done
+                Log.i("timer status", "finished");
             }
         };
 
