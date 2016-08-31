577002a20bceca4a780cc4561ebdc5fb4d78c720
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-30 18:19:05 -0700

    countdown timer

--- app/src/main/java/com/example/clarkhinchcliff/timerdemo/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/timerdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.clarkhinchcliff.timerdemo;
 
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,15 +13,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Handler handler = new Handler();
-        Runnable run = new Runnable() {
-            @Override
-            public void run() {
-                //code to be run every second
-                Log.i("Runnable has run", "One second passed");
-                handler.postDelayed(this, 1000);
+        new CountDownTimer(10000, 1000){
+
+            public void onTick(long msTilDone){
+                Log.i("milliseconds left", String.valueOf(msTilDone));
+            }
+
+            public void onFinish(){
+                //countdown done
             }
         };
-        handler.post(run);
+
+//        final Handler handler = new Handler();
+//        Runnable run = new Runnable() {
+//            @Override
+//            public void run() {
+//                //code to be run every second
+//                Log.i("Runnable has run", "One second passed");
+//                handler.postDelayed(this, 1000);
+//            }
+//        };
+//        handler.post(run);
     }
 }
