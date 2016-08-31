e0f5580a9e85c3e08983020e24ca74baf27ac857
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-30 18:13:56 -0700

    runnables

--- app/src/main/java/com/example/clarkhinchcliff/timerdemo/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/timerdemo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.clarkhinchcliff.timerdemo;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                //code to be run every second
+                Log.i("Runnable has run", "One second passed");
+                handler.postDelayed(this, 1000);
+            }
+        };
     }
 }
